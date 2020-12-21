b0b7fe76a4202b36ef2cfb9579cc75a83e12a8b9
Batuhan Öztürk <34206290+batuhanoztrk@users.noreply.github.com>
2020-12-21 22:14:23 +0300

    Update DataTableButtons.vue

--- components/DataTableButtons.vue
+++ components/DataTableButtons.vue
@@ -1,142 +1,117 @@
 <template>
-  <div>
-    <DataTableButtons
-      v-if="showButtons"
-      @create-click="$emit('create-click')"
-      @delete-click="$emit('delete-click')"
-      :create-text="createText"
-      :delete-text="deleteText"
-      :delete-disabled="deleteDisabled"
-      :create-disabled="createDisabled"
-      :delete-button="deleteButton"
-      :create-button="createButton"
-      :class-name="buttonsClassName"
-      :create-icon="createIcon"
-      :delete-icon="deleteIcon"
-      :delete-message="deleteMessage"
-      :download-button="downloadButton"
-      :download-text="downloadText"
-      :file-name="fileName"
-    />
-    <vue-good-table
-      ref="table"
-      @on-selected-rows-change="$emit('on-selected-rows-change', $event)"
-      @on-cell-click="$emit('on-cell-click', $event)"
-      :select-options="_selectOptions"
-      :search-options="_searchOptions"
-      :sort-options="_sortOptions"
-      :pagination-options="_paginationOptions"
-      @on-row-dblclick="$emit('on-row-dblclick', $event)"
-      @on-row-click="$emit('on-row-click', $event)"
-      :columns="columns"
-      :rows="rows"
+  <div :id="_id" :class="className" class="button-group d-flex">
+    <div>
+      <button
+        type="button"
+        v-if="createButton"
+        :disabled="createDisabled"
+        @click="$emit('create-click')"
+        class="btn waves-effect waves-light btn-success mb-1"
+      >
+        <i :class="createIcon" class="mr-1"></i>
+        {{ _createText }}
+      </button>
+      <button
+        type="button"
+        v-if="deleteButton"
+        :disabled="deleteDisabled"
+        @click="deleteClick"
+        class="btn waves-effect waves-light btn-danger mb-1"
+      >
+        <i :class="deleteIcon" class="mr-1"></i>
+        {{ _deleteText }}
+      </button>
+    </div>
+    <button
+      type="button"
+      @click="download"
+      v-if="downloadButton"
+      class="btn waves-effect waves-light btn-info mb-1 ml-auto"
     >
-      <div slot="emptystate" class="vgt-center-align vgt-text-disabled">
-        {{ noDataText ? noDataText : $t("datatable.noData") }}
-      </div>
-
-      <template slot="table-row" slot-scope="props">
-        <slot name="table-row" :props="props"></slot>
-      </template>
-    </vue-good-table>
+      <i class="fas fa-cloud-download-alt mr-1"></i>
+      {{ _downloadText }}
+    </button>
   </div>
 </template>
 
 <script>
-import DataTableButtons from "@/components/DataTableButtons";
-
 export default {
-  name: "DataTable",
-  components: { DataTableButtons },
+  name: "DataTableButtons",
   props: {
-    selectOptions: Object,
-    searchOptions: Object,
-    sortOptions: Object,
-    paginationOptions: Object,
-    columns: {
-      type: Array,
-      required: true,
-      default: () => {
-        return [];
-      },
-    },
-    rows: {
-      type: Array,
-      required: true,
-      default: () => {
-        return [];
-      },
-    },
-    noDataText: String,
-    showButtons: { type: Boolean, default: true },
-    buttonsClassName: String,
+    className: { type: String, default: "m-2" },
     createText: String,
     deleteText: String,
+    downloadText: String,
     deleteDisabled: Boolean,
-    createDisabled: { type: Boolean, default: false },
+    createDisabled: Boolean,
     deleteButton: { type: Boolean, default: true },
     createButton: { type: Boolean, default: true },
-    downloadButton: Boolean,
-    downloadText: String,
-    fileName: String,
-    createIcon: String,
-    deleteIcon: String,
-    deleteMessage: String,
-    clear: Boolean,
-  },
-  watch: {
-    clear() {
-      this.$refs["table"].unselectAllInternal();
+    downloadButton: { type: Boolean, default: false },
+    fileName: { type: String, default: "Veriler" },
+    createIcon: { type: String, default: "fas fa-plus" },
+    deleteIcon: { type: String, default: "fas fa-trash-alt" },
+    deleteMessage: {
+      type: String,
+      default: function () {
+        return this.$t("deleteMessage");
+      },
     },
   },
-  data() {
-    return {
-      dataTable: {
-        searchOptions: {
-          enabled: true,
-          skipDiacritics: true,
-          placeholder: this.$i18n.t("datatable.search"),
+  methods: {
+    deleteClick() {
+      this.$confirm({
+        message: this.deleteMessage,
+        button: {
+          no: this.$t("dialog.cancel"),
+          yes: this.$t("dialog.ok"),
         },
-        paginationOptions: {
-          enabled: true,
-          mode: "pages",
-          nextLabel: this.$i18n.t("datatable.pagination.next"),
-          prevLabel: this.$i18n.t("datatable.pagination.prev"),
-          rowsPerPageLabel: this.$i18n.t("datatable.pagination.rowsPerPage"),
-          ofLabel: this.$i18n.t("datatable.pagination.of"),
-          pageLabel: this.$i18n.t("datatable.pagination.page"),
-          allLabel: this.$i18n.t("datatable.pagination.all"),
+        callback: (confirm) => {
+          if (confirm) {
+            this.$emit("delete-click");
+          }
         },
-        selectOptions: {
-          enabled: true,
-          selectionText: this.$i18n.t("datatable.selectionText"),
-          clearSelectionText: this.$i18n.t("datatable.clear"),
-          selectOnCheckboxOnly: true,
-        },
-        sortOptions: {
-          enabled: true,
-        },
-      },
-    };
+      });
+    },
+    download() {
+      let ignoreColumn = [];
+      let ignoreRow = [];
+      const bannedLabels = ["img", "button", "input"];
+
+      const firstRow = $(
+        `#${this._id} + .vgt-wrap .vgt-table tbody tr:first-child >`
+      );
+
+      for (let i = 0; i < firstRow.length; i++) {
+        const isBannedLabel = bannedLabels.some((e) =>
+          firstRow[i].innerHTML.includes(e)
+        );
+
+        if (isBannedLabel) ignoreColumn.push(i);
+      }
+
+      if ($(`#${this._id} + .vgt-wrap .vgt-table thead tr`).length > 1)
+        ignoreRow.push(1);
+
+      $(`#${this._id} + .vgt-wrap .vgt-table`).tableExport({
+        type: "csv",
+        fileName: this.fileName,
+        ignoreColumn: ignoreColumn,
+        ignoreRow: ignoreRow,
+      });
+    },
   },
   computed: {
-    _selectOptions() {
-      return this.selectOptions
-        ? this.selectOptions
-        : this.dataTable.selectOptions;
+    _createText() {
+      return this.createText ? this.createText : this.$i18n.t("create");
     },
-    _searchOptions() {
-      return this.searchOptions
-        ? this.searchOptions
-        : this.dataTable.searchOptions;
+    _deleteText() {
+      return this.deleteText ? this.deleteText : this.$i18n.t("deleteSelected");
     },
-    _sortOptions() {
-      return this.sortOptions ? this.sortOptions : this.dataTable.sortOptions;
+    _downloadText() {
+      return this.downloadText ? this.downloadText : this.$i18n.t("download");
     },
-    _paginationOptions() {
-      return this.paginationOptions
-        ? this.paginationOptions
-        : this.dataTable.paginationOptions;
+    _id() {
+      return this.$uuid();
     },
   },
 };
